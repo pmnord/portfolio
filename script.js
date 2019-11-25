2f60259d0ce8b650e213e28f6a3374c3ef007e29
Peter Nordman <petermnordman@gmail.com>
2019-11-25 12:56:57 -0500

    working first version

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.smoothScroll = function(target) {
     scroll = function(c, a, b, i) {
         i++; if (i > 30) return;
         c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+        setTimeout(function(){ scroll(c, a, b, i); }, 8);
     }
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
@@ -24,3 +24,22 @@ window.smoothScroll = function(target) {
 const chevron = document.querySelector('.my-chevron');
 const projects = document.querySelector('.projects');
 chevron.addEventListener('click', () => {window.smoothScroll(projects)});
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {makeHeaderSticky()};
+
+// Get the header
+var header = document.querySelector("header");
+
+// Get the offset position of the header
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function makeHeaderSticky() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky");
+  }
+}
