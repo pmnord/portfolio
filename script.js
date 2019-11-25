6f22e9876aa94d6deba232fb019aa3bedd2affc8
Peter Nordman <petermnordman@gmail.com>
2019-11-25 10:53:15 -0500

    prepare for pull

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-var frmvalidator = new Validator(“contactform”); 
-frmvalidator.addValidation(“name”,”req”,”Please provide your name”); 
-frmvalidator.addValidation(“email”,”req”,”Please provide your email”); 
-frmvalidator.addValidation(“email”,”email”, “Please enter a valid email address”);
-=======
 window.smoothScroll = function(target) {
     var scrollContainer = target;
     do { //find scroll container
@@ -30,4 +24,3 @@ window.smoothScroll = function(target) {
 const chevron = document.querySelector('.my-chevron');
 const projects = document.querySelector('.projects');
 chevron.addEventListener('click', () => {window.smoothScroll(projects)});
->>>>>>> 4990fc44e167da462aaf763e535ae9e04f031b36
